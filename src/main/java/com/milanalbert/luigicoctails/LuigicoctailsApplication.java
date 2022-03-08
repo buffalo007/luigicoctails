67dcde1923f00ffe076b22e6ee22a63ca5a2d312
ovp007 <m.albertbb@gmail.com>
2022-03-08 11:21:43 +0100

    init commit

---
+++ src/main/java/com/milanalbert/luigicoctails/LuigicoctailsApplication.java
@@ -0,0 +1,13 @@
+package com.milanalbert.luigicoctails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuigicoctailsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuigicoctailsApplication.class, args);
+    }
+
+}
